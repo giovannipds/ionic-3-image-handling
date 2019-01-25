3e928fc496aa417f49bfbdfe5313995d639524b0
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-01-25 16:26:01 -0200

    Add methods (even still seeing errors)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ImagePicker } from '@ionic-native/image-picker';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  photos
 
+  constructor(
+    public navCtrl: NavController,
+    public imagePicker: ImagePicker,
+    public cropService: Crop,
+  ) {
+
+  }
+
+  openImagePicker(){
+    let options= {
+      maximumImagesCount: 5,
+    }
+    this.photos = new Array<string>();
+    this.imagePicker.getPictures(options)
+    .then((results) => {
+      this.reduceImages(results).then(() => {
+        console.log('all images cropped!!');
+      });
+    }, (err) => { console.log(err) });
+  }
+
+  reduceImages(selected_pictures: any) : any{
+    return selected_pictures.reduce((promise:any, item:any) => {
+      return promise.then((result) => {
+        return this.cropService.crop(item, {quality: 75})
+        .then(cropped_image => this.photos.push(cropped_image));
+      });
+    }, Promise.resolve());
   }
 
 }
